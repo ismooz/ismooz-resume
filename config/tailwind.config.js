3f1ffe5307ee2aa7ba44dae71648ac20c7f50263
ismooz <i.gashi@pm.me>
2022-01-23 22:49:23 +0100

    Added shared logo for skills

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,25 +11,25 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      // animation: {
-      //   blob: "blob 4s infinite"
-      // },
-      // keyframes: {
-      //   blob: {
-      //     "0%" : {
-      //       transform: "translate(0px, 0px) scale(1)"
-      //     },
-      //     "33%" : {
-      //       transform: "translate(30px, -50px) scale(1.2)"
-      //     },
-      //     "66%" : {
-      //       transform: "translate(-20px, 20px) scale(0.8)"
-      //     },
-      //     "100%" : {
-      //       transform: "translate(0px, 0px) scale(1)"
-      //     },
-      //   },
-      // },
+      animation: {
+        blob: "blob 7s infinite"
+      },
+      keyframes: {
+        blob: {
+          "0%" : {
+            transform: "translate(0px, 0px) scale(1)"
+          },
+          "33%" : {
+            transform: "translate(30px, -50px) scale(1.2)"
+          },
+          "66%" : {
+            transform: "translate(-20px, 20px) scale(0.8)"
+          },
+          "100%" : {
+            transform: "translate(0px, 0px) scale(1)"
+          },
+        },
+      },
     },
   },
   plugins: [
